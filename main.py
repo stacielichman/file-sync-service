e93af78ea20dbfc9966715fcb9717ae72165f74d
Анастасия Личман <stacie.lichman@gmail.com>
2024-02-16 19:12:28 +0300

    done

---
+++ main.py
@@ -0,0 +1,135 @@
+from __future__ import annotations
+
+import time
+
+from loguru import logger
+import os
+import threading
+from datetime import datetime
+from os.path import getctime
+from threading import Lock
+from typing import Iterable
+
+from routes import SYNC_FOLDER_PATH, CloudFiles, UploadFiles
+
+
+LOCK: threading.Lock = Lock()
+run_thread = True
+
+
+def get_local_dir_files() -> dict:
+    """
+    Gets all files from the local directory
+    :return: dict
+    """
+    try:
+        dir_dict: dict = {
+            file: datetime.fromtimestamp(
+                getctime(SYNC_FOLDER_PATH + '/' + file))
+            .strftime('%Y-%m-%dT%H:%M:34+00:00')
+            for file in os.listdir(SYNC_FOLDER_PATH)}
+    except BaseException as exc:
+        logger.error(f'{type(exc).__name__}')
+        global run_thread
+        run_thread = False
+    else:
+        return dir_dict
+
+
+def get_cloud_files() -> dict:
+    """
+    Gets all files from the cloud directory
+    :return: dict
+    """
+    cloud_files = CloudFiles().get()
+    if cloud_files.status_code != 200:
+        logger.error(cloud_files.json()["message"])
+        global run_thread
+        run_thread = False
+    else:
+        cloud_files = cloud_files.json()
+        file_path = cloud_files["_embedded"]["items"]
+        cloud_dict: dict = {
+            file["name"]: file["modified"]
+            for file in file_path}
+        return cloud_dict
+
+
+def check_if_delete_file() -> None:
+    """
+    Compares and deletes extra files in the cloud directory
+    :return: None
+    """
+    with LOCK:
+        cloud_files = list(get_cloud_files().keys())
+        dir_files = list(get_local_dir_files().keys())
+
+        for file in dir_files:
+            to_delete: Iterable = filter(lambda cloud_file: cloud_file != file, cloud_files)
+            cloud_files = list(to_delete)
+        files_to_delete: list = cloud_files
+
+        if len(files_to_delete) == 0:
+            return
+        CloudFiles().delete(files_to_delete)
+
+
+def check_if_upload_file() -> None:
+    """
+    Compares and uploads files into cloud directory
+    :return: None
+    """
+    with LOCK:
+        cloud_files = list(get_cloud_files().keys())
+        dir_files = list(get_local_dir_files().keys())
+
+        for file in cloud_files:
+            to_upload: Iterable = filter(lambda dir_file: dir_file != file, dir_files)
+            dir_files = list(to_upload)
+        files_to_upload = list(dir_files)
+
+        if len(files_to_upload) == 0:
+            return
+        UploadFiles().get(files_to_upload)
+
+
+def check_if_reload_file() -> None:
+    """
+    Compares the time the files were modified
+    and rewrites files into cloud directory
+    :return: None
+    """
+    with LOCK:
+        cloud_files: dict = get_cloud_files()
+        dir_files: dict = get_local_dir_files()
+
+        files_to_patch: list = []
+        for filename, mod_time in dir_files.items():
+            if filename in cloud_files:
+                if cloud_files[filename] != mod_time:
+                    files_to_patch.append(filename)
+        UploadFiles().get(files_to_patch)
+
+
+def main():
+    while run_thread:
+        time.sleep(10)
+        thread_1 = threading.Thread(target=check_if_delete_file)
+        thread_2 = threading.Thread(target=check_if_upload_file)
+        thread_3 = threading.Thread(target=check_if_reload_file)
+
+        thread_1.start()
+        thread_2.start()
+        thread_3.start()
+
+        thread_1.join()
+        thread_2.join()
+        thread_3.join()
+
+
+if __name__ == '__main__':
+    logger.info(
+        f"Программа синхронизации файлов начинает "
+        f"работу с директорией {SYNC_FOLDER_PATH}.")
+    main()
+
